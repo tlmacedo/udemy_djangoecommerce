aba48727ec29da25e56f8dd724d6a9235b332c2b
Thiago Macedo <tl.macedo@hotmail.com>
2017-10-23 14:58:03 -0400

    auto commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contact, name='contact'),
+    url(r'^produto/$', views.product, name='product'),
+    url(r'^produtos/$', views.product_list, name='product_list'),
+]
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -16,12 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from core import views
-
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^contato/$', views.contact, name='contact'),
-    url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/$', views.product_list, name='product_list'),
+    url(r'^', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
